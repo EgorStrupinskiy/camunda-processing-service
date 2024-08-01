416bb0612c802ad603501b12bc5e8c58d8c1e657
egor <egor.strupinskiy@gmail.com>
2024-08-01 16:51:51 +0300

    Initial commit

---
+++ src/test/java/com/azati/warshipprocessing/WarshipProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azati.warshipprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarshipProcessingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
